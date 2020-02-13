17fa87a728c518b234c0e2de3d543cf713795277
Sergei Sergeev <sergeev.srg@gmail.com>
2020-02-13 19:29:34 +0300

    Initial

---
+++ src/webparts/helloWorld/components/HelloWorld.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import styles from './HelloWorld.module.scss';
+import { IHelloWorldProps } from './IHelloWorldProps';
+import { escape } from '@microsoft/sp-lodash-subset';
+import { getContext } from '../HelloWorldWebPart';
+
+export default class HelloWorld extends React.Component<IHelloWorldProps, {}> {
+  public render(): React.ReactElement<IHelloWorldProps> {
+    const helloWorldContext = getContext();
+
+    return (
+      <div className={ styles.helloWorld }>
+        <div className={ styles.container }>
+          <div className={ styles.row }>
+            <div className={ styles.column }>
+              <p className={ styles.subTitle }>WebPart Id: </p>
+              <p className={ styles.description }>{helloWorldContext.instanceId}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
